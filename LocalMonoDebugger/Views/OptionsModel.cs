f0c040268fac3ff7f13902c52f336d382350da29
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-01-19 11:26:39 -0600

    Added model for options

---
+++ LocalMonoDebugger/Views/OptionsModel.cs
@@ -0,0 +1,32 @@
+﻿using LocalMonoDebugger.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalMonoDebugger.Views
+{
+    public class OptionsModel : BaseViewModel
+    {
+        private OptionsContainer _container = OptionsManager.Instance.Load();
+        public OptionsContainer Container
+        {
+            get => _container;
+            set
+            {
+                _container = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public void Save()
+            => OptionsManager.Instance.Save(Container);
+
+        public void LoadFrom(string path)
+            => Container = OptionsManager.Instance.LoadFromPath(path);
+
+        public void SaveTo(string path)
+            => OptionsManager.Instance.SaveAs(Container, path);
+    }
+}
